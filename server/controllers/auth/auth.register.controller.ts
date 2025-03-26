29748a3237e0b28be8a354b80703ad5cd35f1eab
davidg999 <davyd.arutiunian@gmail.com>
2025-03-26 16:56:11 +0200

    feat: handle tokens on register

--- server/controllers/auth/auth.register.controller.ts
+++ server/controllers/auth/auth.register.controller.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
 import { z } from "zod";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import { authSchema } from "schemas/auth.schema.ts";
 import User from "models/user.model.ts";
+import {
+  ACCESS_TOKEN_EXPIRES_IN,
+  REFRESH_TOKEN_EXPIRES_IN,
+  refreshTokenCookieOptions,
+} from "configs/auth.config.ts";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -16,12 +22,28 @@ export const register = async (req: Request, res: Response) => {
 
     const hashedPassword = await bcrypt.hash(parsedPayload.password, 10);
 
-    await User.create({
+    const newUser = await User.create({
       ...parsedPayload,
       password: hashedPassword,
     });
 
-    return res.status(201).json({ message: "User created successfully." });
+    const accessToken = jwt.sign(
+      { id: newUser._id, username: newUser.username },
+      process.env.ACCESS_TOKEN_SECRET!,
+      { expiresIn: ACCESS_TOKEN_EXPIRES_IN },
+    );
+    const refreshToken = jwt.sign(
+      { id: newUser._id },
+      process.env.REFRESH_TOKEN_SECRET!,
+      { expiresIn: REFRESH_TOKEN_EXPIRES_IN },
+    );
+
+    newUser.refreshToken = refreshToken;
+    await newUser.save();
+
+    res.cookie("refreshToken", refreshToken, refreshTokenCookieOptions);
+
+    return res.status(201).json({ accessToken });
   } catch (error) {
     if (error instanceof z.ZodError) {
       return res.status(400).json({
