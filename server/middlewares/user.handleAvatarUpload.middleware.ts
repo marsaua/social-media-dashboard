6255b766a10f7ca5edf8f0f3b7be0f6b5bc6fd0f
davidg999 <davyd.arutiunian@gmail.com>
2025-04-24 12:21:19 +0300

    feat: bootstrap nestjs app

--- server/middlewares/user.handleAvatarUpload.middleware.ts
+++
@@ -1,8 +0,0 @@
-import multer from "multer";
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-const handleAvatarUpload = upload.single("avatar");
-
-export default handleAvatarUpload;
