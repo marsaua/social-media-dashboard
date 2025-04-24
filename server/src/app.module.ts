a2093ba8162dadabffe6ca0b41c684328b370492
davidg999 <davyd.arutiunian@gmail.com>
2025-04-24 16:17:24 +0300

    feat: add environment config; setup TypeORM

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from 'src/config/app.config';
+import dbConfig from 'src/config/db.config';
+import environmentValidation from 'src/config/environment.validation';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      load: [appConfig, dbConfig],
+      validationSchema: environmentValidation,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        autoLoadEntities: configService.get('db.autoLoadEntities'),
+        database: configService.get('db.name'),
+        host: configService.get('db.host'),
+        password: configService.get('db.password'),
+        port: configService.get('db.port'),
+        synchronize: configService.get('db.synchronize'),
+        type: 'postgres',
+        username: configService.get('db.username'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/config/environment.validation.ts
@@ -0,0 +1,12 @@
+import * as Joi from 'joi';
+
+export default Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test', 'staging')
+    .default('development'),
+  DATABASE_PORT: Joi.number().port().default(5432),
+  DATABASE_USERNAME: Joi.string().required(),
+  DATABASE_PASSWORD: Joi.string().required(),
+  DATABASE_HOST: Joi.string().required(),
+  DATABASE_NAME: Joi.string().required(),
+});
